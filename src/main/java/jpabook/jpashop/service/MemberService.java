4d1e2103fed9685d809ce7b57994526d4a737985
rineun <sy880808@gmail.com>
2021-04-20 11:25:11 +0900

    test

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -2,20 +2,27 @@ package jpabook.jpashop.service;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+//spring이 가져있는 Transactional
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
-public class MenberService {
-    @Autowired
-    private MemberRepository memberRepository;
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
 
     /**
      *
      회원가입
      */
+    @Transactional
     public Long join(Member member){
         validationDuplicateMember(member); // 중복회원검
         memberRepository.save(member);
@@ -23,6 +30,7 @@ public class MenberService {
     }
 
     private void validationDuplicateMember(Member member) {
+        //DB name unique 제약조건 넣어야함
         List<Member> findMembers = memberRepository.findByName(member.getName());
         if(!findMembers.isEmpty()){
             throw new IllegalStateException("이미 존재하는 회원입니다.");
@@ -36,7 +44,6 @@ public class MenberService {
     }
 
     //회원 한건 조회
-
     public Member findOne(Long memberId){
         return memberRepository.findOne(memberId);
     }
